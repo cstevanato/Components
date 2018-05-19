c883c857ee210745041b68c875b71f12e4fedc31
Claudio Stevanato <cstevanato@gmail.com>
2018-05-19 16:55:41 -0300

    Atuliazando versão do Kotlin

---
+++ app/src/main/java/br/com/stv/components/TestCircleTextViewActivity.kt
@@ -0,0 +1,12 @@
+package br.com.stv.components
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestCircleTextViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_circle_text_view)
+    }
+}
